60fa4e8629c475eb8f98019dbc253d9182c3fab4
Vinicius Pereira <vini.gpereira2014@gmail.com>
2022-08-24 18:32:27 -0300

    chore: makes the test run smoothly inside the container

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from utils.file import createFiles, deleteDirectory
 import docker
 import os
 
+DEBUG = False
+
 DOCKER_JAVASCRIPT_JEST_IMAGE_NAME = 'vinigpereira/javascript-jest:latest'
-DOCKER_JAVASCRIPT_JEST_TEST_COMMAND = 'jest tmp'
+DOCKER_JAVASCRIPT_JEST_TEST_COMMAND = 'jest'
 DOCKER_CONTAINER_WORKDIR = '/sandbox'
 TMP_DIRECTORY_NAME = 'tmp'
 TMP_SANDBOX_DIRECTORY_PATH = f"{DOCKER_CONTAINER_WORKDIR}/tmp"
@@ -26,8 +28,11 @@ async def run(runRequest: RunRequest):
   logStream = None
 
   try:
+    if DEBUG: print("Creating docker client")
     client = docker.from_env()
+    if DEBUG: print("Pulling images")
     client.images.pull(DOCKER_JAVASCRIPT_JEST_IMAGE_NAME)
+    if DEBUG: print("Running javascript container")
     container = client.containers.run(
       DOCKER_JAVASCRIPT_JEST_IMAGE_NAME,
       DOCKER_JAVASCRIPT_JEST_TEST_COMMAND,
@@ -35,8 +40,14 @@ async def run(runRequest: RunRequest):
       working_dir=DOCKER_CONTAINER_WORKDIR,
       volumes=[f"{tmpDirectoryPath}:{TMP_SANDBOX_DIRECTORY_PATH}"]
     )
+    if DEBUG: print("Saving the logstream")
     logStream = container.logs(stream=True);
+    if DEBUG: print("Stopping container")
+    container.stop()
+    if DEBUG: print("Removing container")
+    container.remove()    
   finally:
+    if DEBUG: print("Deleting tmp directory")
     deleteDirectory(tmpDirectoryPath, runRequest.files)
   
   finalLog = ""
@@ -45,6 +56,6 @@ async def run(runRequest: RunRequest):
     while True:
       finalLog += next(logStream).decode("utf-8")
   except StopIteration:
-    print("log iteration stopped")
+    if DEBUG: print("log iteration stopped")
 
   return finalLog
