2ea4dd8b7cc03eb5eba3dd90383e464485400a38
Vinicius Pereira <vini.gpereira2014@gmail.com>
2022-09-19 00:11:36 -0300

    chore: add security options on docker run

--- services/docker_service.py
+++ services/docker_service.py
@@ -1,10 +1,14 @@
+from typing import List, Tuple
 import env
 
-from docker import DockerClient
+from docker import DockerClient, types
 from utils.docker_container import DockerContainer
 from models.docker_image_name import DockerImageName
 from builders.docker_container_builder import DockerContainerBuilder
 from services.docker_image_name_service import DockerImageNameService
+
+KB = 1024
+MB = 1024 * KB
 
 class DockerService():
   def __init__(
@@ -32,6 +36,26 @@ class DockerService():
       testCommand,
       detach=True,
       working_dir=env.DOCKER_WORKDIR,
-      volumes=[f"{env.PROJECT_TMP_DIRECTORY}:{env.DOCKER_TMP_DIRECTORY}"]
+      volumes=[f"{env.PROJECT_TMP_DIRECTORY}:{env.DOCKER_TMP_DIRECTORY}"],
+      network_mode="none",
+      pids_limit=128,
+      kernel_memory="768m",
+      mem_limit="768m",
+      ulimits=self.getUlimits([
+        ('core', 0),
+        ('fsize', 128*MB),
+        ('locks', 1024),
+        ('nofile', 1024),
+        ('nproc', 1024),
+        ('stack', 16*MB),
+      ])
     )
     return self.dockerContainerBuilder.buildContainer(container)
+
+  def getUlimits(self, ulimitDefinitions: List[Tuple[str, int]]) -> List[types.Ulimit]:
+    ulimits: List[types.Ulimit] = [];
+
+    for name, value in ulimitDefinitions:
+      ulimits.append(types.Ulimit(name=name, soft=value, hard=value))
+    
+    return ulimits
