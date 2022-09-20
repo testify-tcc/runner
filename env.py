215f18b4a4952abbd4344b2c4891c290f88672b7
Vinicius Pereira <vini.gpereira2014@gmail.com>
2022-09-19 22:07:53 -0300

    fix: client prod endpoint

--- env.py
+++ env.py
@@ -2,7 +2,7 @@ import os
 
 DOCKER_HUB_PROFILE = "vinigpereira"
 CLIENT_DEV_ENDPOINT = "http://localhost:8080"
-CLIENT_PROD_ENDPOINT = "https://imeusp-testify.netlify.app/"
+CLIENT_PROD_ENDPOINT = "http://testify.surge.sh"
 PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 DOCKER_WORKDIR = "/sandbox"
 DOCKER_TMP_DIRECTORY = f"{DOCKER_WORKDIR}/tmp"
